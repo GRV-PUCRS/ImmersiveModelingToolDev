c56a7ef69227723486441880df0a6cfb15de2eb1
Vinicius Chrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2023-10-11 09:00:49 -0300

    start implements socket object

---
+++ Assets/Scripts/SocketObjectManager/ITrackebleObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f9970f8190057293639524890069caa5477713a0f7dd5271273e038a9bd3c796
+size 548
---
+++ Assets/Scripts/SocketObjectManager/SocketObject.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:450b4858bd27746129fff8c80fe23e2f1fad580486c10f1d935fb1d88d6b4f54
+size 506
