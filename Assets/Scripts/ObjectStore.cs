9057e499547909cf9e3e91e855d7d6ff968d999f
Vinicius Chrisosthemos <vinicius.teixeira.002@acad.pucrs.br>
2023-11-24 14:41:17 -0300

    Change fixed object size

--- Assets/Scripts/ObjectStore.cs
+++ Assets/Scripts/ObjectStore.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:208c320313bbb21be9c34841ce259c5e5a3f79ad04ab2e6541670b106d448ea8
-size 13243
+oid sha256:f0b52ade2e7b1cfc96b910b4c2873a43580bf97e19e33c5b78aa3e3aebdc6a24
+size 13420
---
+++ Assets/Scripts/Others/ModelSizeHandler.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:de735a2f68c7251309e7cbc2202f955130c880c9200b5df182dab9ed22c90d8f
+size 241
